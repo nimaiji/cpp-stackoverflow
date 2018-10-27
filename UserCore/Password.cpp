48a96e4262a261fc546876f453ffebc698867bc3
nima <ijinima@gmail.com>
2018-10-27 18:06:30 +0330

    Password class

---
+++ UserCore/Password.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nima iji on 10/27/2018 AD.
+//
+
+#include "Password.h"
---
+++ UserCore/Password.h
@@ -0,0 +1,14 @@
+//
+// Created by Nima iji on 10/27/2018 AD.
+//
+
+#ifndef CPP_STACKOVERFLOW_PASSWORD_H
+#define CPP_STACKOVERFLOW_PASSWORD_H
+
+
+class Password {
+
+};
+
+
+#endif //CPP_STACKOVERFLOW_PASSWORD_H
