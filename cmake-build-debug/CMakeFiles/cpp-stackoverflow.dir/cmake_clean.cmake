ee260c08828926a3ed7b0b3c89b830cb9c65e45d
parham <bghparham@gmail.com>
2018-10-27 15:24:17 +0330

    database first commit

---
+++ cmake-build-debug/CMakeFiles/cpp-stackoverflow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp-stackoverflow.dir/main.cpp.o"
+  "cpp-stackoverflow.pdb"
+  "cpp-stackoverflow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp-stackoverflow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/cpp-stackoverflow.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cpp-stackoverflow.
+# This may be replaced when dependencies are built.
